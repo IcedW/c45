11609f81c445b4a80935b48070a9a79b6ce64707
IcedW <117010225+IcedW@users.noreply.github.com>
2025-03-13 16:07:03 +0100

    Add files via upload

---
+++ CL 1/Work1/Work1/Work1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ///*cout << &main << "\n";
+    //cout << &cout << "\n";*/
+    //int age = 3;
+    //int cats = 3;
+    //double dogs = 2.2;
+    //double teeth = 6.5;
+    //string name = "ball";
+    ////name = "ball445"; //char
+    //cout << teeth << "\n";
+
+    cout << "wsg ";
+    string answer;
+    cin >> answer; //input
+    cout << "alr idrc.. " << answer << " o-oh my..~" << "\n";
+
+    const double PI = 3.1415926;
+    PI = 4;
+
+}
